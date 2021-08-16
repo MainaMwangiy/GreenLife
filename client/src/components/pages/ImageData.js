91e498a449565981066633e67e125837e5916fe9
fastlinker <charlesmaina8848@gmail.com>
2021-08-16 17:02:11 +0300

    updates

--- client/src/components/pages/ImageData.js
+++ client/src/components/pages/ImageData.js
@@ -15,99 +15,142 @@ import trash_3 from "../images/trash-3.jpg"
 import trees from "../images/trees.jpg"
 import watering from "../images/watering.jpg"
 import green_environ from "../images/green-environ.jpg"
+const text = "GreenLife";
+const text1 = "GreenLife";
+const text2 = "GreenLife";
+const text3 = "GreenLife";
+const text4 = "GreenLife";
+const text5 = "GreenLife";
+const text6 = "GreenLife";
+const text7 = "GreenLife";
+const text8 = "GreenLife";
 
 export const ImageData = [
-    {
-      id: 1,
-      image:
-        img1
-    },
-    {
-      id: 2,
-      image:
-        coll_1
-    },
-    {
-      id: 3,
-      image:
-        coll_2
-    },
-    {
-      id: 4,
-      image:
-        collec
-    },
-    {
-      id: 5,
-      image:
-        education
-    },
-    {
-      id: 6,
-      image:
-        field
-    },{
+  {
+    id: 1,
+    image:
+      img1,
+    text: text
+  },
+  {
+    id: 2,
+    image:
+      coll_1
+    ,
+    text: text1
+  },
+  {
+    id: 3,
+    image:
+      coll_2
+    ,
+    text: text1
+  },
+  {
+    id: 4,
+    image:
+      collec
+    ,
+    text: text1
+  },
+  {
+    id: 5,
+    image:
+      education
+    ,
+    text: text1
+  },
+  {
+    id: 6,
+    image:
+      field
+    ,
+    text: text1
+  }, {
     id: 7,
     image:
       fummig
+    ,
+    text: text1
   },
   {
     id: 8,
     image:
       green_environ
+    ,
+    text: text1
   },
   {
     id: 9,
     image:
       plant
+    ,
+    text: text1
   },
   {
     id: 10,
     image:
       planting
+    ,
+    text: text1
   },
   {
     id: 11,
     image:
       planting_trees
+    ,
+    text: text1
   },
   {
     id: 12,
     image:
       trash_1
+    ,
+    text: text1
   },
   {
     id: 13,
     image:
       trash_2
+    ,
+    text: text1
   }
   ,
   {
     id: 14,
     image:
       trash_3
+    ,
+    text: text1
   }
   ,
   {
     id: 15,
     image:
       sanitary
+    ,
+    text: text1
   }
   ,
   {
     id: 16,
     image:
       trees
-  }  ,
+    ,
+    text: text1
+  },
   {
     id: 17,
     image:
       watering
-  }  ,
+    ,
+    text: text1
+  },
   {
     id: 18,
     image:
       trees
+    ,
+    text: text1
   }
-  ];
-  
+];
--- client/src/components/pages/ImageSlider.js
+++ client/src/components/pages/ImageSlider.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { ImageData } from "./ImageData"
+import { Carousel } from "react-bootstrap";
 
 const ImageSlider = ({ slides }) => {
   const [current, setCurrent] = useState(0);
@@ -16,29 +17,54 @@ const ImageSlider = ({ slides }) => {
   if (!Array.isArray(slides) || slides.length <= 0) {
     return null;
   }
+  const images = ImageData;
   return (
-    <section className='slider'>
-      <button className=" left-arrow" type="button" onClick={prevSlide}  >
-        <span className="visually-hidden"> <i className="fa fa-angle-left" aria-hidden="true"></i> </span>
-      </button>
-      <button className=" right-arrow" type="button" onClick={nextSlide} >
-        <span className="carousel-control-next-icon" aria-hidden="true"></span>
-        <span className="visually-hidden"> <i className="fa fa-angle-right" aria-hidden="true"></i> </span>
+    // <section className='slider'>
+    //   <button className=" left-arrow" type="button" onClick={prevSlide}  >
+    //     <span className="visually-hidden"> <i className="fa fa-angle-left" aria-hidden="true"></i> </span>
+    //   </button>
+    //   <button className=" right-arrow" type="button" onClick={nextSlide} >
+    //     <span className="carousel-control-next-icon" aria-hidden="true"></span>
+    //     <span className="visually-hidden"> <i className="fa fa-angle-right" aria-hidden="true"></i> </span>
 
-      </button>
-      {ImageData.map((slide, index) => {
+    //   </button>
+    //   {ImageData.map((slide, index) => {
+    //     return (
+    //       <div
+    //         className={index === current ? 'slide active' : 'slide'}
+    //         key={index}
+    //       >
+    //         {index === current && (
+    //           <img
+    //             src={slide.image}
+    //             alt='travel'
+    //             className='image' />
+
+    //         )}
+    //       </div>
+    //     );
+    //   })}
+    // </section>
+    <Carousel
+      ssr
+      partialVisbile
+      itemClass="image-item"
+    >
+      {images.map(image => {
         return (
-          <div
-            className={index === current ? 'slide active' : 'slide'}
-            key={index}
-          >
-            {index === current && (
-              <img src={slide.image} alt='travel' className='image' />
-            )}
+          <div>
+            <img
+              draggable={false}
+              alt="text"
+              style={{ width: "100%", height: "100%" }}
+              src={image.image}
+            />
+            {/* Have to style so this should see over the image */}
+            <span style={{color:"red", fontSize:"5rem"}}>{image.text}</span>
           </div>
         );
       })}
-    </section>
+    </Carousel>
 
   );
 }
