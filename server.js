482532ea9f3dcba156c2e2c283fdfcb68c8a1b80
fastlinker <charlesmaina8848@gmail.com>
2021-09-03 17:13:33 +0300

    additions

--- server.js
+++ server.js
@@ -84,11 +84,3 @@ if (process.env.NODE_ENV === 'production') {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
-
-// app.get('/*', function(req, res) {   
-//     res.sendFile(path.join(__dirname, 'path/to/your/index.html'), function(err) {
-//       if (err) {
-//         res.status(500).send(err)
-//       }
-//     })
-//   })
