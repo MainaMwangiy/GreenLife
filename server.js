e8e8c3757eb1f237f1ebc755f0b2439d75958c06
fastlinker <charlesmaina8848@gmail.com>
2021-09-04 15:08:03 +0300

    pkg adds

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ require('dotenv').config()
 const express = require("express");
 const router = express.Router();
 const cors = require("cors");
+const path = require('path');
 const nodemailer = require("nodemailer");
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 app.use(express.static('client/build'))
