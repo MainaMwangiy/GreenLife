b30265df030b722fe1ccf767ccfef58210d41e58
fastlinker <charlesmaina8848@gmail.com>
2021-09-05 19:48:22 +0300

    updates

--- server.js
+++ server.js
@@ -86,3 +86,11 @@ if (process.env.NODE_ENV === 'production') {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
+// serve static assets normally
+app.use(express.static(__dirname + '/public'))
+
+// handle every other route with index.html, which will contain
+// a script tag to your application's JavaScript file(s).
+app.get('*', function (request, response){
+  response.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+})
